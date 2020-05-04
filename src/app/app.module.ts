cf4524747a66bdcd034bafe2c6e4863e21927a69
Rafal <rmf@europe.com>
2020-05-04 07:06:00 +0200

    basic setting for the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { OneComponent } from './one/one.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+export const appRouter: Routes = [
+  {path: '', component: OneComponent},
+  {path: 'one', component: OneComponent},
+  {path: '**', component: OneComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OneComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRouter),
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/one/one.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-one',
+  templateUrl: './one.component.html',
+  styleUrls: ['./one.component.css']
+})
+export class OneComponent implements OnInit {
+
+  subTitle: string = 'Fetching data from JSON file';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
