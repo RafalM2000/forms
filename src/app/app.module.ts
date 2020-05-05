d908d5310435d53c595a951a69d1e2c42e54a6bb
Rafal <rmf@europe.com>
2020-05-05 06:51:43 +0200

    material buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 import { OneComponent } from './one/one.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 export const appRouter: Routes = [
   {path: '', component: OneComponent},
@@ -21,6 +23,7 @@ export const appRouter: Routes = [
   imports: [
     BrowserModule,
     MatInputModule,
+    MatButtonModule,
     RouterModule.forRoot(appRouter),
     BrowserAnimationsModule,
   ],
