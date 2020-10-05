2b49b0c9032aa6b8ba076ecb42c7193394504c6d
Rafał Mytych <rfm@europe.com>
2020-10-05 14:01:02 +0200

    new layout and component two

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component,DoCheck } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements DoCheck {
   title1: string = 'Angular-In-Examples';
   title2: string = 'Forms';
+  title3: string = '';
+
+  constructor(private currentRoute: Router) {}
+
+  ngDoCheck() {
+    switch (this.currentRoute.url) {
+      case '/one':
+        this.title3 = 'Template driven forms';
+        break;
+      case '/two':
+        this.title3 = 'Reactive driven forms';
+        break;
+      default:
+        this.title3 = '';
+    }
+  }
+
 }
+
+
